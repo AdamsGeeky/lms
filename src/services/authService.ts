0921afecbaa7609f90d67895a34c258fd02d058f
Founder Geekink <muhdadamu0008@gmail.com>
2026-01-17 18:04:50 +0100

    chore init

--- src/services/authService.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../models/User';
-import { IUser } from '../types/authTypes';
-
-export class AuthService {
-  static async login(email: string, password: string): Promise<IUser | null> {
-    const user = await User.findOne({ email, password });
-    return user ? user : null;
-  }
-}
